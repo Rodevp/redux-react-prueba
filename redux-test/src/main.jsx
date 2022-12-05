4c0a2944009506df20e239b50610c0b48d8d11e0
Rodevp <rodriguezrodrigor12@gmail.com>
2022-12-05 17:14:54 -0500

    create redux store and slices

--- redux-test/src/main.jsx
+++ redux-test/src/main.jsx
@@ -4,8 +4,14 @@ import App from './App'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
+//Redux
+import { Provider } from 'react-redux'
+import store from './app/store'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
